082e9cb5fdabc1395ce6ec554f2a434a68cf93d0
Admin <ulanapolancikova@gmail.com>
2025-02-04 23:50:12 +0300

    module1

---
+++ module1.py
@@ -0,0 +1,15 @@
+GAME_NAME = 'our game'
+ICONE_IMAGE_WAY = 'files/icon.png'
+DEAD_IMAGE_WAY = 'files/dead.png'
+COIN_IMAGE_WAY = 'files/coin.png'
+GRESS_IMAGE_WAY = 'files/gress.jpg'
+WATER_IMAGE_WAY = 'files/gress.jpg'
+START_BUTTON = 'Start'
+EXIT_BUTTON = 'Exit'
+BACK_IMAGE_WAY = 'files/result_a5439e1.jpg'
+BACK1_IMAGE_WAY = 'files/back1.jpg'
+BULLET_IMAGE_WAY = 'files/bullet.png'
+DOOR_IMAGE_WAY = 'files/door.png'
+RULES_BUTTON = 'Rules'
+RESUME_BUTTON = "Resume"
+
